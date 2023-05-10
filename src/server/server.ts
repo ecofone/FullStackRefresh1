c2f979a1ce12da77ede6f5938eb92114fa9096fa
Enrique Cofone <enrique.cofone@gmail.com>
2023-05-10 18:12:51 +0200

    module 1 completed

--- src/server/server.ts
+++ src/server/server.ts
@@ -7,7 +7,9 @@ server.use(express.static("dist"));
 server.set("view engine", "ejs");
 
 server.use("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    initialContent: "Loading....",
+  });
 });
 
 server.listen(PORT, HOST, () => {
